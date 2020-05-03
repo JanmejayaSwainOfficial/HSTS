d41c1495a05206d912503fce175282d6159d0997
JanmejayaSwainOfficial <31533983+JanmejayaSwainOfficial@users.noreply.github.com>
2020-05-03 23:42:41 +0530

    Add files via upload

---
+++ hsts.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+echo "###################################################"
+echo "#                                                 #"
+echo "#         This Tool is made for to check HSTS     #"
+echo "#            syntax:- https//exaple.com           #"
+echo "#                                                 #"
+echo "###################################################"
+
+read -p  "Enter URL : " URL
+
+curl -s -D- $URL |grep -i Strict
